3e9cd49d8f304c92100fae45b2c991a0afd1a8ff
Marq Gray <marquis.s.sandifer@gmail.com>
2021-08-24 21:17:40 -0500

    Populated transactions in DOM list

--- script.ts
+++ script.ts
@@ -14,13 +14,29 @@ const app = new (class {
     text: document.getElementById("text"),
     amount: document.getElementById("amount"),
   };
-
   dummyTransactions: Transaction[] = [
     { id: 1, text: "Flower", amount: -20 },
     { id: 2, text: "Salary", amount: 300 },
     { id: 3, text: "Book", amount: -10 },
     { id: 4, text: "Camera", amount: 150 },
   ];
-
   transactions = this.dummyTransactions;
+
+  constructor() {
+    this.htmlElements.list.innerHTML = "";
+    this.transactions.forEach((transaction) => {
+      this.addTransactionDOM(transaction);
+    });
+  }
+
+  addTransactionDOM(transaction: Transaction) {
+    const sign = transaction.amount < 0 ? "-" : "+";
+    const item = document.createElement("li");
+    item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+    item.innerHTML = `
+      ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+      <button class="delete-btn">x</button>
+    `;
+    this.htmlElements.list.appendChild(item);
+  }
 })();
