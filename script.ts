30d2c7b48b2f5935aca3981ec3fd11ac67db6770
Marq Gray <marquis.s.sandifer@gmail.com>
2021-08-24 21:02:49 -0500

    Imported DOM elements

--- script.ts
+++ script.ts
@@ -1,1 +1,11 @@
-const app = new (class {})();
+const app = new (class {
+  htmlElements = {
+    balance: document.getElementById("balance"),
+    moneyDisplayPlus: document.getElementById("money-plus"),
+    moneyDisplayMinus: document.getElementById("money-minus"),
+    list: document.getElementById("list"),
+    form: document.getElementById("form"),
+    text: document.getElementById("text"),
+    amount: document.getElementById("amount"),
+  };
+})();
