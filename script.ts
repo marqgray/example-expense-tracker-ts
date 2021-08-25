22399d168ed1a2566fde42bc15178893195473cc
Marq Gray <marquis.s.sandifer@gmail.com>
2021-08-24 21:51:06 -0500

    Added functionality to value income and expense displays

--- script.ts
+++ script.ts
@@ -43,4 +43,22 @@ const app = new (class {
     `;
     this.htmlElements.list.appendChild(item);
   }
+
+  updateValues() {
+    const amounts = this.transactions.map((transaction) => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const income = amounts
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+    const expense = (
+      amounts
+        .filter((item) => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1
+    ).toFixed(2);
+
+    this.htmlElements.balance.innerText = `$${total}`;
+    this.htmlElements.moneyDisplayPlus.innerText = `$${income}`;
+    this.htmlElements.moneyDisplayMinus.innerText = `$${expense}`;
+  }
 })();
